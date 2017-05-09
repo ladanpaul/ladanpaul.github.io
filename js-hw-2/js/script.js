83848747707d2a831bbb2933559c27387456e12c
ladanpaul <ladanpaul87@gmail.com>
2017-05-09 20:02:00 +0300

    added js-hw-2

--- js-hw-2/js/script.js
+++ js-hw-2/js/script.js
@@ -1,19 +1,24 @@
 var arr = [];
-console.log(arr.length);
 
 for (var i = 0; i < 5; i++) {
   var names = prompt('Введите имя');
-  var names = arr.push(names);
+  arr.push(names);
 }
 
 console.log(arr);
 
 var yourName = prompt('Введите имя для проверки');
 
+var trigger = 0;
+
 for(var i = 0; i < arr.length; i++) {
-  if (arr[i] === yourName) {
-    alert('Ваше имя: ', yourName);
-  } else {
-    alert('Нет такого имени');
+  if (arr[i] == yourName) {
+    trigger++;
   }
+}
+
+if (trigger > 0) {
+  alert("Ваше имя " + yourName);
+} else {
+  alert('Такого имени не существует');
 }
