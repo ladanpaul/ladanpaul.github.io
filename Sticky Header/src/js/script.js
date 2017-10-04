4fa666a3eb2b95a8599228bb56163ca37539efec
ladanpaul <ladanpaul87@gmail.com>
2017-10-04 04:20:53 +0300

    added Sticky Header

---
+++ Sticky Header/src/js/script.js
@@ -0,0 +1,17 @@
+// $(document).on('scroll', function(){
+//   var navTop = $(this).scrollTop();
+//   if(navTop >= 450) {
+//     $('.nav-menu').addClass('nav-fixed');
+//   } else {
+//     $('.nav-menu').removeClass('nav-fixed');
+//   }
+// });
+
+document.addEventListener('scroll', function(){
+  var navTop = window.pageYOffset || document.documentElement.scrollTop;
+  if(navTop >= 450){
+    document.querySelector('.nav-menu').classList.add('nav-fixed');
+  } else {
+    document.querySelector('.nav-menu').classList.remove('nav-fixed');    
+  }
+});
